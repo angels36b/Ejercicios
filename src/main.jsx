7ff524921211385b5bb0c5f252b34d818be68b7a
angel <angel.36b@gmail.com>
2022-07-11 23:47:35 -0500

    configuracion de package-json

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+//estilos
+import './styles/index.css'
+
+//libreria
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//componentes
+import App from './componentes/App'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
